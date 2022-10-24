e638c1d3b412fe0b8ae877142e92d22fafabccf6
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2022-10-24 12:29:23 +0530

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,18 @@
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observor) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  sectionObservor.unobserve(entry.target);
+};
+
+const sectionObservor = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach(function (section) {
+  sectionObservor.observe(section);
+  section.classList.add("section--hidden");
+});
