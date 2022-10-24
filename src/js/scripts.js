8ea276171664f047334afd4a06b2da49cc420223
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2022-10-24 13:35:48 +0530

    minor fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,18 +1,18 @@
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 
-const revealSection = function (entries, observor) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  sectionObservor.unobserve(entry.target);
-};
+// const revealSection = function (entries, observor) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove("section--hidden");
+//   sectionObservor.unobserve(entry.target);
+// };
 
-const sectionObservor = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.1,
-});
+// const sectionObservor = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.1,
+// });
 
-allSections.forEach(function (section) {
-  sectionObservor.observe(section);
-  section.classList.add("section--hidden");
-});
+// allSections.forEach(function (section) {
+//   sectionObservor.observe(section);
+//   section.classList.add("section--hidden");
+// });
