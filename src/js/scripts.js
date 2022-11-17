6a43b3cbd62eb2a1c98d6c8dd0fb140952498f1a
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2022-11-17 19:47:36 +0530

    added gallery

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,34 @@
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 
-const revealSection = function (entries, observor) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  sectionObservor.unobserve(entry.target);
-};
+// const revealSection = function (entries, observor) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove("section--hidden");
+//   sectionObservor.unobserve(entry.target);
+// };
 
-const sectionObservor = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.1,
-});
+// const sectionObservor = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.1,
+// });
 
-allSections.forEach(function (section) {
-  sectionObservor.observe(section);
-  section.classList.add("section--hidden");
-});
+// allSections.forEach(function (section) {
+//   sectionObservor.observe(section);
+//   section.classList.add("section--hidden");
+// });
 
-const btnTranslate = document.getElementById("translate");
-const sectionEN = document.getElementsByClassName("english")[0];
-const sectionHB = document.getElementsByClassName("hebrew")[0];
-console.log(btnTranslate, sectionEN, sectionHB);
+// const btnTranslate = document.getElementById("translate");
+// const sectionEN = document.getElementsByClassName("english")[0];
+// const sectionHB = document.getElementsByClassName("hebrew")[0];
+// console.log(btnTranslate, sectionEN, sectionHB);
 
-btnTranslate.addEventListener("click", () => {
-  if (btnTranslate.textContent == "Translate to עִברִית") {
-    btnTranslate.textContent = "Translate to English";
-  } else {
-    btnTranslate.textContent = "Translate to עִברִית";
-  }
+// btnTranslate.addEventListener("click", () => {
+//   if (btnTranslate.textContent == "Translate to עִברִית") {
+//     btnTranslate.textContent = "Translate to English";
+//   } else {
+//     btnTranslate.textContent = "Translate to עִברִית";
+//   }
 
-  sectionEN.classList.toggle("hidden");
-  sectionHB.classList.toggle("hidden");
-});
+//   sectionEN.classList.toggle("hidden");
+//   sectionHB.classList.toggle("hidden");
+// });
