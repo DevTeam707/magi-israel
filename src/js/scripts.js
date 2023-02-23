1f81e1ce1228c2ec07d242ab2eab735d7960c6de
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2023-02-23 11:29:53 +0530

    website 2.0

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,21 +1,46 @@
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 
-const revealSection = function (entries, observor) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  sectionObservor.unobserve(entry.target);
+// const revealSection = function (entries, observor) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove("section--hidden");
+//   sectionObservor.unobserve(entry.target);
+// };
+
+// const sectionObservor = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.1,
+// });
+
+// allSections.forEach(function (section) {
+//   sectionObservor.observe(section);
+//   section.classList.add("section--hidden");
+// });
+
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init("gP6L_j8HqKiURVM4z");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      console.log(this);
+      emailjs.sendForm("service_tpkxkl6", "template_9oi5huv", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
 };
-
-const sectionObservor = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.1,
-});
-
-allSections.forEach(function (section) {
-  sectionObservor.observe(section);
-  section.classList.add("section--hidden");
-});
 
 const btnTranslate = document.getElementById("translate");
 const sectionEN = document.getElementsByClassName("english")[0];
